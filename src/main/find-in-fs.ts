9219a92bb0528b729509b12aa98ada31d93a7b34
Aviv Ben Shahar <avivbens87@gmail.com>
2024-06-11 05:16:13 +0300

    fix: add global error handler

--- src/main/find-in-fs.ts
+++ src/main/find-in-fs.ts
@@ -12,80 +12,84 @@ import { searchInFileSystem } from '@services/search.service'
     const sliceAmount = alfredClient.env.getEnv(Variables.SLICE_AMOUNT, { defaultValue: 10, parser: Number })
     const targetType = alfredClient.env.getEnv<SearchOptions['type']>(Variables.TARGET_TYPE, { defaultValue: 'all' })
 
-    /**
-     * Advanced options
-     */
-    const pathsParser = (data: string) =>
-        data
-            .trim()
-            .split('\n')
-            .filter(Boolean)
-            .map((path) => path.replace('~', homedir()))
+    try {
+        /**
+         * Advanced options
+         */
+        const pathsParser = (data: string) =>
+            data
+                .trim()
+                .split('\n')
+                .filter(Boolean)
+                .map((path) => path.replace('~', homedir()))
 
-    const onlyIn = alfredClient.env.getEnv(Variables.ONLY_IN, {
-        defaultValue: [],
-        parser: pathsParser,
-    })
-    const forceInclude = alfredClient.env.getEnv(Variables.FORCE_INCLUDE, {
-        defaultValue: [],
-        parser: pathsParser,
-    })
-    const excludePaths = alfredClient.env.getEnv(Variables.EXCLUDE_PATHS, {
-        defaultValue: [],
-        parser: pathsParser,
-    })
+        const onlyIn = alfredClient.env.getEnv(Variables.ONLY_IN, {
+            defaultValue: [],
+            parser: pathsParser,
+        })
+        const forceInclude = alfredClient.env.getEnv(Variables.FORCE_INCLUDE, {
+            defaultValue: [],
+            parser: pathsParser,
+        })
+        const excludePaths = alfredClient.env.getEnv(Variables.EXCLUDE_PATHS, {
+            defaultValue: [],
+            parser: pathsParser,
+        })
 
-    const excludeGit = alfredClient.env.getEnv(Variables.EXCLUDE_GIT, {
-        defaultValue: false,
-        parser: (data) => Boolean(Number(data)),
-    })
-    const excludeHidden = alfredClient.env.getEnv(Variables.EXCLUDE_HIDDEN, {
-        defaultValue: false,
-        parser: (data) => Boolean(Number(data)),
-    })
-    const gitReposOnly = alfredClient.env.getEnv(Variables.GIT_REPOS_ONLY, {
-        defaultValue: false,
-        parser: (data) => Boolean(Number(data)),
-    })
-    const excludeNodeModules = alfredClient.env.getEnv(Variables.EXCLUDE_NODE_MODULES, {
-        defaultValue: false,
-        parser: (data) => Boolean(Number(data)),
-    })
+        const excludeGit = alfredClient.env.getEnv(Variables.EXCLUDE_GIT, {
+            defaultValue: false,
+            parser: (data) => Boolean(Number(data)),
+        })
+        const excludeHidden = alfredClient.env.getEnv(Variables.EXCLUDE_HIDDEN, {
+            defaultValue: false,
+            parser: (data) => Boolean(Number(data)),
+        })
+        const gitReposOnly = alfredClient.env.getEnv(Variables.GIT_REPOS_ONLY, {
+            defaultValue: false,
+            parser: (data) => Boolean(Number(data)),
+        })
+        const excludeNodeModules = alfredClient.env.getEnv(Variables.EXCLUDE_NODE_MODULES, {
+            defaultValue: false,
+            parser: (data) => Boolean(Number(data)),
+        })
 
-    const { input } = alfredClient
+        const { input } = alfredClient
 
-    const res = await searchInFileSystem({
-        name: input,
-        type: targetType,
-        onlyIn,
-        forceInclude,
-        excludePaths,
-        /*  */
-        excludeGit,
-        excludeHidden,
-        gitReposOnly,
-        excludeNodeModules,
-    })
+        const res = await searchInFileSystem({
+            name: input,
+            type: targetType,
+            onlyIn,
+            forceInclude,
+            excludePaths,
+            /*  */
+            excludeGit,
+            excludeHidden,
+            gitReposOnly,
+            excludeNodeModules,
+        })
 
-    const items: AlfredScriptFilter['items'] = res.map(({ path, type }) => {
-        const fileName = basename(path)
+        const items: AlfredScriptFilter['items'] = res.map(({ path, type }) => {
+            const fileName = basename(path)
 
-        const icon =
-            type === 'folder'
-                ? alfredClient.icons.getIcon('genericFolder')
-                : alfredClient.icons.getIcon('genericDocument')
+            const icon =
+                type === 'folder'
+                    ? alfredClient.icons.getIcon('genericFolder')
+                    : alfredClient.icons.getIcon('genericDocument')
 
-        return {
-            title: fileName,
-            subtitle: path,
-            arg: path,
-            icon: { path: icon },
-            uid: path,
-            type: 'file:skipcheck',
-        }
-    })
+            return {
+                title: fileName,
+                subtitle: path,
+                arg: path,
+                icon: { path: icon },
+                uid: path,
+                type: 'file:skipcheck',
+            }
+        })
 
-    const sliced = items.slice(0, sliceAmount)
+        const sliced = items.slice(0, sliceAmount)
 
-    alfredClient.output({ items: sliced, cache: { seconds: 60 * 20 } })
+        alfredClient.output({ items: sliced, cache: { seconds: 60 * 20 } })
+    } catch (error) {
+        alfredClient.error(error)
+    }
 })()
